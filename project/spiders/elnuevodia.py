3a06cff941405f9f4dd7976a83964dcb532f46fd
Hernán <hernan.carvajal@javerianacali.edu.co>
2020-06-03 17:53:55 -0500

    Version inicial del proyecto

---
+++ project/spiders/elnuevodia.py
@@ -0,0 +1,32 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from project.items import News
+
+
+class ElNuevoDiaSpider(scrapy.Spider):
+    name = "elnuevodia"
+
+    def start_requests(self):
+        urls = [
+            'http://www.elnuevodia.com.co/nuevodia/actualidad/judicial',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+      container = response.xpath('//div[@class="view-content"]')
+      for url in container.xpath('//article//header//a/@href').getall():
+        next_page = response.urljoin(url)
+        yield scrapy.Request(url=next_page, callback=self.read_news)
+      
+    def read_news(self, response):
+      titulo = response.xpath('//div[@id="block-pagetitle"]//h1/span/text()').get()
+      cuerpo = response.xpath('//div[@class="node-content"]//p//text()').getall()
+      hora   = response.xpath('//div[@class="node-content"]//time/@datetime').get()
+
+      news = ItemLoader(item=News())
+      news.add_value('titulo', titulo)
+      news.add_value('cuerpo', cuerpo)
+      news.add_value('hora', hora)
+      return news.load_item()
+      
---
+++ project/spiders/lanacion_spyder.py
@@ -0,0 +1,30 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from project.items import News
+
+
+class LaNacionSpider(scrapy.Spider):
+    name = "lanacion"
+
+    def start_requests(self):
+        urls = [
+            'https://www.lanacion.com.co/category/judicial/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+      container = response.xpath('//div[@class="penci-archive__list_posts"]')
+      for url in container.xpath('//article//div[@class="penci-pmore-link"]/a/@href').getall():
+        yield scrapy.Request(url=url, callback=self.read_news)
+      
+    def read_news(self, response):
+      titulo = response.xpath('//header//h1/text()').get()
+      cuerpo = response.xpath('//article//div/p/text()').getall()
+      hora   = response.xpath('//article//time/@datetime').get()
+      
+      news = ItemLoader(item=News())
+      news.add_value('titulo', titulo)
+      news.add_value('cuerpo', cuerpo)
+      news.add_value('hora', hora)
+      return news.load_item()
