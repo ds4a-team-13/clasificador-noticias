670cadfa13f1b75cbb68a95bcbccef427efdd31a
Hernán <hernan.carvajal@javerianacali.edu.co>
2020-07-03 14:20:44 -0500

    Improved spiders

--- data/reading_example.py
+++ data/reading_example.py
@@ -1,7 +1,16 @@
-import sqlite3
-import pandas as pd
+#%%
 
-cnx = sqlite3.connect('data.db')
+import pandas as pd
+import sqlite3
+
+cnx = sqlite3.connect('../data/data.db')
 df = pd.read_sql_query("SELECT * FROM news", cnx)
 
-print(df.head())
+df.groupby('diario').agg({'fecha_publicacion':['min', 'max'], 'url':'size'})
+
+#%%
+
+import pandas as pd
+
+df = pd.read_csv('news.csv')
+df['url'].apply(lambda x: x.split('.com')[0]).value_counts()
